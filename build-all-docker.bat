174943c7e06bd46ed4a12b4f7707e29b7542486e
drescherflo <drescherflo@protonmail.com>
2022-11-30 16:04:17 +0100

    Add Dockerfiles and build configs for local build

---
+++ build-all-docker.bat
@@ -0,0 +1,6 @@
+
+docker build -f cad-AuthService\Dockerfile -t cad-auth cad-AuthService\
+docker build -f cad-GymService\Dockerfile -t cad-gym cad-GymService\
+docker build -f cad-UserService\Dockerfile -t cad-user cad-UserService\
+docker build -f cad-WorkoutService\Dockerfile -t cad-workout cad-WorkoutService\
+docker build -f cad-Frontend\Dockerfile -t cad-frontend cad-Frontend\
