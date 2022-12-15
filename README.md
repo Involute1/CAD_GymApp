955d33517284fc8491d05ed768f7732f788719f4
Luis.Nothvogel <L_noth@yahoo.de>
2022-12-15 15:10:19 +0100

    working on tf configs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CAD_GymApp
+
+## Google docs
+
+https://docs.google.com/document/d/1vOt_h6hP7h5gIU5vKxFKVm9rNN8fhSYNcrghxI3CjmA/edit?usp=sharing
--- terraform/modules/variables.tf
+++ terraform/modules/variables.tf
@@ -1,3 +1,11 @@
 variable "project_id" {
   type = string
 }
+
+variable "eu-zone" {
+  type = string
+}
+
+variable "eu-region" {
+  type = string
+}
