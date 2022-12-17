82c023b6750f39f03d450bdba4007d61d3ea5a3f
Luis.Nothvogel <L_noth@yahoo.de>
2022-12-17 19:41:43 +0100

    removed tf tokens added run configs

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "remote" {
     organization = "HTWG_CAD_GYM"
-    //TODO put sensitive info somewhere safe
-    token        = "7z0DFubhq7r0gA.atlasv1.1UvaZdFXgW6ksa2qyzNsZ7BrlyEzfAWqAklkN7vNNhMqPh6tnwv5dnWjLskIwTlGXrQ"
+
     workspaces {
       name = "CAD_GymApp"
     }
