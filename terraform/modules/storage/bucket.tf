2bb2447cb925de4898a8c9e0953221be512649e7
Luis.Nothvogel <L_noth@yahoo.de>
2022-12-21 16:40:57 +0100

    tf removed random id from bucket

--- terraform/modules/storage/bucket.tf
+++ terraform/modules/storage/bucket.tf
@@ -1,9 +1,5 @@
-resource "random_id" "bucket_prefix" {
-  byte_length = 8
-}
-
 resource "google_storage_bucket" "gym-bucket" {
-  name          = "${random_id.bucket_prefix.hex}-gym-bucket-${var.environment}"
+  name          = "gym-bucket-${var.environment}"
   project       = var.project_id
   location      = var.eu_location
   force_destroy = true
