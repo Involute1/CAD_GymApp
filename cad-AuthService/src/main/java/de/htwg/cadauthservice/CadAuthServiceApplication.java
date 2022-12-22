f3f12f8c85903dc917cdba5ea583edb291f82914
Luis.Nothvogel <L_noth@yahoo.de>
2022-12-22 13:52:54 +0100

    auth works now with freshly generated key file

--- cad-AuthService/src/main/java/de/htwg/cadauthservice/CadAuthServiceApplication.java
+++ cad-AuthService/src/main/java/de/htwg/cadauthservice/CadAuthServiceApplication.java
@@ -1,36 +1,15 @@
 package de.htwg.cadauthservice;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-
 @SpringBootApplication
 public class CadAuthServiceApplication {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(CadAuthServiceApplication.class, args);
-        initFirebaseConfig();
-    }
-
-    public static void initFirebaseConfig() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("W:\\HTWG_Master\\CAD_GymApp\\cad-AuthService\\src\\main\\resources\\cad-project-368216-10a1836bc192.json");
-//        FileInputStream serviceAccount = new FileInputStream("W:\\HTWG_Master\\CAD_GymApp\\cad-AuthService\\src\\main\\resources\\authKey.json");
-
-
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setProjectId("cad-project-368216")
-                .build();
-
-        FirebaseApp.initializeApp(options);
     }
 
     @Bean
--- terraform/modules/services/service-account.tf
+++ terraform/modules/services/service-account.tf
@@ -10,21 +10,13 @@ resource "google_project_iam_binding" "tf_service_account_iam_binding_toolkit" {
   role    = "roles/identitytoolkit.admin"
 }
 
-//use this for the key for auth
 resource "google_service_account_key" "tf_account_key" {
   service_account_id = google_service_account.tf_account.name
   public_key_type    = "TYPE_X509_PEM_FILE"
 }
 
 resource "local_sensitive_file" "tf_account_key" {
-  filename = "${path.root}/../../../cad-AuthService/src/main/resources/authKey.json"
-  content  = jsonencode({
-    "private_key"    = "-----BEGIN PRIVATE KEY-----\n${google_service_account_key.tf_account_key.private_key}\n-----END PRIVATE KEY-----\n",
-    "type"           = "service_account",
-    "private_key_id" = substr(google_service_account_key.tf_account_key.id, -40, -1),
-    "client_email"   = google_service_account.tf_account.email,
-    "client_id"      = google_service_account.tf_account.id
-  })
-  #  content  = "{\n\"type\":\"service_account\", \n\"project_id\":\"${var.project_id}\", \n\"private_key_id\":\"${substr(google_service_account_key.tf_account_key.id, -40, -1)}\", \n\"private_key\":\"-----BEGIN PRIVATE KEY-----${google_service_account_key.tf_account_key.private_key}\n-----END PRIVATE KEY-----\n\", \n\"client_email\":\"${google_service_account.tf_account.email}\", \n\"client_id\":\"${google_service_account.tf_account.id}\", \n\"auth_uri\":\"https://accounts.google.com/o/oauth2/auth\", \n\"token_uri\":\"https://oauth2.googleapis.com/token\", \n\"auth_provider_x509_cert_url\":\"https://www.googleapis.com/oauth2/v1/certs\", \n\"client_x509_cert_url\":\"https://www.googleapis.com/robot/v1/metadata/x509/dev123456789%40cad-project-368216.iam.gserviceaccount.com\"\n}"
+  filename = "${path.root}/../../../cad-AuthService/src/main/resources/tf_service_account_key.json"
+  content  = base64decode(google_service_account_key.tf_account_key.private_key)
 }
 
