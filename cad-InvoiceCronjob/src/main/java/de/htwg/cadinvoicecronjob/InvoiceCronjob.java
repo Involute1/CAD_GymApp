02f43e6a39d8ab6493611e4759d900b7ef057540
drescherflo <drescherflo@protonmail.com>
2023-01-18 23:54:10 +0100

    - Add correct calculation for enterprise price in InvoiceCronjob - Fix missing PathVariable-Annotation in GoogleAuthUserController

--- cad-InvoiceCronjob/src/main/java/de/htwg/cadinvoicecronjob/InvoiceCronjob.java
+++ cad-InvoiceCronjob/src/main/java/de/htwg/cadinvoicecronjob/InvoiceCronjob.java
@@ -3,6 +3,7 @@ package de.htwg.cadinvoicecronjob;
 import com.google.gson.Gson;
 import de.htwg.cadinvoicecronjob.model.Gym;
 import de.htwg.cadinvoicecronjob.model.Invoice;
+import de.htwg.cadinvoicecronjob.model.User;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -17,6 +18,7 @@ import java.util.List;
 
 public class InvoiceCronjob {
     private static final Log LOGGER = LogFactory.getLog(InvoiceCronjob.class);
+    private static final double COST_PER_USER = 0.3; // TODO: env?
 
     public static void main(String[] args) throws IOException, InterruptedException {
         LOGGER.info("Starting Invoice Generation");
@@ -24,6 +26,10 @@ public class InvoiceCronjob {
         String gymServiceName = System.getenv("GYM_SERVICE");
         String gymServiceUrl = "http://" + (gymServiceName == null ? "localhost:7081" : gymServiceName);
         LOGGER.debug("Gym Service URL: " + gymServiceUrl);
+
+        String userServiceName = System.getenv("GYM_SERVICE");
+        String userServiceUrl = "http://" + (userServiceName == null ? "localhost:7082" : userServiceName);
+        LOGGER.debug("User Service URL: " + userServiceUrl);
 
         LOGGER.info("Requesting gyms");
         HttpClient httpClient = HttpClient.newBuilder().build();
@@ -44,7 +50,24 @@ public class InvoiceCronjob {
             double amount = switch (gym.getBillingModel()) {
                 case FREE: yield 0;
                 case STANDARD: yield 100;
-                case ENTERPRISE: yield 1000; // TODO: Use user count in calculation
+                case ENTERPRISE: {
+                    LOGGER.info("Requesting users for gym");
+                    HttpRequest usersRequest = HttpRequest.newBuilder()
+                            .header("Content-Type", "application/json")
+                            .GET()
+                            .uri(URI.create(userServiceUrl + "/all/" + gym.getTenantId()))
+                            .build();
+                    HttpResponse<String> usersResponse;
+                    try {
+                        usersResponse = httpClient.send(usersRequest, HttpResponse.BodyHandlers.ofString());
+                    } catch (IOException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    LOGGER.info("Unmarshalling response");
+                    User[] users = gson.fromJson(usersResponse.body(), User[].class);
+                    long userCount = Arrays.stream(users).filter(user -> !user.isDisabled()).count();
+                    yield userCount * COST_PER_USER;
+                }
             };
             return new Invoice(null, LocalDateTime.now(), amount, LocalDateTime.now().plusDays(3), gym.getFirebaseId());
         }).toList();
