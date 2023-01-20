49a372dcf11b09bd13f8c05e34d8f2607aa543a9
drescherflo <drescherflo@protonmail.com>
2023-01-20 12:21:37 +0100

    Move helper scripts from project root to helper dir

--- helper/deploy-to-kubernetes.bat
+++ helper/deploy-to-kubernetes.bat
@@ -4,10 +4,10 @@ terraform apply -auto-approve
 
 cd ../../../
 
-docker build -t europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-gym-service:latest cad-GymService/
-docker build -t europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-user-service:latest cad-UserService/
-docker build -t europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-workout-service:latest cad-WorkoutService/
-docker build -t europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-frontend:latest cad-Frontend/
+docker build -t europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-gym-service:latest ../cad-GymService/
+docker build -t europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-user-service:latest ../cad-UserService/
+docker build -t europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-workout-service:latest ../cad-WorkoutService/
+docker build -t europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-frontend:latest ../cad-Frontend/
 
 docker push europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-gym-service:latest
 docker push europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-user-service:latest
@@ -17,4 +17,4 @@ docker push europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-fronten
 helm upgrade --install ingress-nginx ingress-nginx \
   --repo https://kubernetes.github.io/ingress-nginx \
   --namespace ingress-nginx --create-namespace
-helm upgrade --install gymapp ./cad-gymapp
+helm upgrade --install gymapp ../cad-gymapp
