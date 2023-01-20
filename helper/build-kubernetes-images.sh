eccf47771f82be07a7fb3cabfc425f4b75ce946a
drescherflo <drescherflo@protonmail.com>
2023-01-20 19:24:56 +0100

    Fix tf apply on macOS

--- helper/build-kubernetes-images.sh
+++
@@ -1,4 +0,0 @@
-docker build -t europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-gym-service:latest ../cad-GymService/
-docker build -t europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-user-service:latest ../cad-UserService/
-docker build -t europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-workout-service:latest ../cad-WorkoutService/
-docker build -t europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-frontend:latest ../cad-Frontend/
--- helper/push-kubernetes-images.sh
+++
@@ -1,4 +0,0 @@
-docker push europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-gym-service:latest
-docker push europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-user-service:latest
-docker push europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-workout-service:latest
-docker push europe-west3-docker.pkg.dev/cad-gym-app/cad-repo-dev/cad-gym-frontend:latest
