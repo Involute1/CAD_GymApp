e8d6f801d3d3018e39c51c634fcf5a4e73bc648d
drescherflo <drescherflo@protonmail.com>
2023-01-22 16:02:18 +0100

    Fix missing uninstallation of ingress-nginx

--- helper/full-uninstall.sh
+++ helper/full-uninstall.sh
@@ -1,5 +1,7 @@
 helm uninstall gymapp
 helm uninstall postgresql-user
 helm uninstall postgresql-workout
+helm uninstall ingress-nginx --namespace ingress-nginx
+kubectl delete namespace ingress-nginx
 kubectl delete pvc --all  # remove persistent volume claims of postgresql (including login credentials etc)
 
