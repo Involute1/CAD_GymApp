ee70bbe3b1d9fcf47702856db32afa259629c085
Luis.Nothvogel <L_noth@yahoo.de>
2023-01-25 01:29:48 +0100

    tf fixes

--- terraform/modules/dns/main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  #  https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/dns_managed_zone
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.49.0"
-    }
-  }
-}
