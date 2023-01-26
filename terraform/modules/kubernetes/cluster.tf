08797fc593a6ce4fc73f10e3ce6d427ccd65b0b1
drescherflo <drescherflo@protonmail.com>
2023-01-26 02:37:29 +0100

    - Add script to run curl shell in namespace

--- terraform/modules/kubernetes/cluster.tf
+++ terraform/modules/kubernetes/cluster.tf
@@ -1,30 +1,16 @@
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
-  location = var.eu_location
+  location = "europe-west3"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = 1
-  #enable_autopilot         = true
-}
-
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = var.node_pool_name
-  location   = var.eu_location
-  cluster    = google_container_cluster.primary.name
-  autoscaling {
-    max_node_count = 4
-    min_node_count = 1
+  #remove_default_node_pool = true
+  #initial_node_count       = 1
+  enable_autopilot         = true
+  ip_allocation_policy {
   }
-
   node_config {
-    preemptible  = true
-    machine_type = var.machine_type
-
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    #    service_account = google_service_account.default.email
     service_account = var.service_account_email
     oauth_scopes    = [
       "https://www.googleapis.com/auth/cloud-platform",
@@ -34,4 +20,31 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
       "https://www.googleapis.com/auth/trace.append"
     ]
   }
+
 }
+
+#resource "google_container_node_pool" "primary_preemptible_nodes" {
+#  name       = var.node_pool_name
+#  location   = var.eu_location
+#  cluster    = google_container_cluster.primary.name
+#  autoscaling {
+#    max_node_count = 4
+#    min_node_count = 1
+#  }
+#
+#  node_config {
+#    preemptible  = true
+#    machine_type = var.machine_type
+#
+#    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+#    #    service_account = google_service_account.default.email
+#    service_account = var.service_account_email
+#    oauth_scopes    = [
+#      "https://www.googleapis.com/auth/cloud-platform",
+#      "https://www.googleapis.com/auth/devstorage.read_only",
+#      "https://www.googleapis.com/auth/servicecontrol",
+#      "https://www.googleapis.com/auth/service.management.readonly",
+#      "https://www.googleapis.com/auth/trace.append"
+#    ]
+#  }
+#}
