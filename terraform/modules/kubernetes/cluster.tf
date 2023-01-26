90423ef295cbb44116688e9a47ef5a9265167c5d
drescherflo <drescherflo@protonmail.com>
2023-01-26 10:08:16 +0100

    Hardcode cluster zone, since terraform seems to cache it somewhere

--- terraform/modules/kubernetes/cluster.tf
+++ terraform/modules/kubernetes/cluster.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
-  location = var.eu_location
+  location = "europe-west3-c"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
